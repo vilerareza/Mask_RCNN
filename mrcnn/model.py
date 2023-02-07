64611503e8df3d7769c1d99bcdf3fa4ed6ff815b
Sanskar Hasija <54099851+sanskar-hasija@users.noreply.github.com>
2023-02-07 05:40:08 +0530

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2362,9 +2362,9 @@ class MaskRCNN(object):
             callbacks=callbacks,
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
-            max_queue_size=100,
-            workers=workers,
-            use_multiprocessing=workers > 1,
+            #max_queue_size=100,
+            #workers=workers,
+            #use_multiprocessing=workers > 1,
         )
         self.epoch = max(self.epoch, epochs)
 
